fce148bfe1d8b708edba0e283920588edddd72ee
Luziberto <luzibertomendesjunior@hotmail.com>
2023-02-16 14:13:35 -0300

    Adjust default coin sync time

--- config/coingecko.php
+++ config/coingecko.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Facade;
 
 return [
     'url' => env('COIN_GECKO_BASE_URL', 'https://api.coingecko.com/api/v3'),
-    'sync_time' => env('COIN_SYNC_TIME', 10)
+    'sync_time' => env('COIN_SYNC_TIME', 20)
 ];
